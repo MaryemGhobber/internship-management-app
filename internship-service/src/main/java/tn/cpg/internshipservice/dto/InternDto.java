8ba565c06d88f7c1a41151943b9d5699d9b8b1aa
maryem <maryem@gmail.com>
2024-02-15 11:27:09 +0100

    update dto itern and internshio

--- internship-service/src/main/java/tn/cpg/internshipservice/dto/InternDto.java
+++ internship-service/src/main/java/tn/cpg/internshipservice/dto/InternDto.java
@@ -1,9 +1,10 @@
 package tn.cpg.internshipservice.dto;
 
 
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import tn.cpg.internshipservice.entities.Internship;
 import tn.cpg.internshipservice.enums.ClassLevel;
 import tn.cpg.internshipservice.enums.Level;
@@ -14,7 +15,7 @@ import java.time.LocalDate;
 /**
  * DTO for {@link tn.cpg.internshipservice.entities.Intern}
  */
-@Getter @Setter
+
 public record InternDto(Long idIntern, int cin, String firstname, String lastname, String mail, int phoneNumber,
                         LocalDate birthdate, Level level, ClassLevel classLevel,
                         Internship internship) implements Serializable {
--- internship-service/src/main/java/tn/cpg/internshipservice/dto/InternshipDto.java
+++ internship-service/src/main/java/tn/cpg/internshipservice/dto/InternshipDto.java
@@ -1,8 +1,8 @@
 package tn.cpg.internshipservice.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.Value;
+
+
+
 import tn.cpg.internshipservice.entities.Intern;
 
 import java.io.Serializable;
@@ -11,6 +11,6 @@ import java.util.List;
 /**
  * DTO for {@link tn.cpg.internshipservice.entities.Internship}
  */
-@Getter @Setter
+
 public record InternshipDto(Long idInternship, List<Intern> internList) implements Serializable {
 }
