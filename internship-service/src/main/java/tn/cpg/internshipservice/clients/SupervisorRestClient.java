bf0140560b481077d9778be921228c26db27474c
maryem <maryem@gmail.com>
2024-03-03 17:57:12 +0100

    add resilience4j (circuit breaker) to internship-service

--- internship-service/src/main/java/tn/cpg/internshipservice/clients/SupervisorRestClient.java
+++ internship-service/src/main/java/tn/cpg/internshipservice/clients/SupervisorRestClient.java
@@ -1,5 +1,6 @@
 package tn.cpg.internshipservice.clients;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +11,19 @@ import java.util.List;
 @FeignClient(name = "SUPERVISOR-SERVICE")
 public interface SupervisorRestClient {
     @GetMapping("/supervisors/{id}")
+    @CircuitBreaker(name = "supervisorService", fallbackMethod = "getDefaultSupervisors")
     Supervisor findSupervisorById(@PathVariable Long id);
 
     @GetMapping("/supervisors")
     List<Supervisor> allSupervisors();
+
+    default Supervisor getDefaultSupervisors(Long id, Exception exception) {
+        Supervisor supervisor = new Supervisor();
+
+        supervisor.setIdSupervisor(id);
+        supervisor.setFirstname("Not Available");
+        supervisor.setLastname("Not Available");
+        supervisor.setMail("Not Available");
+        return supervisor;
+    }
 }
